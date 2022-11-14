07495369e8de41f63721036968ae4cf8d7ad0da9
MarioPr <marioaguirre516@gmail.com>
2022-11-14 09:54:25 +0100

    actualizacion
    actu

---
+++ actualización 14-11-22/11-14-22/listaUsers.php
@@ -0,0 +1,87 @@
+<?php include_once "base.php";  permisosListas();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- BOOTSTRAP LIBRARY -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- HOJA DE ESTILOS -->
+    <link rel="stylesheet" href="css/preloader.css">
+    <link rel="stylesheet" href="css/indexStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<nav id="menu" class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid ">
+            <a class="navbar-brand" href="index.php">
+                <!-- LOGO -->
+                <i class="fas fa-arrow-left" style="color:#565656; font-size: 50px;"></i>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span> <!-- ICONO VERSIÓN MOVIL -->
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul id="menu2" class="navbar-nav">
+                <li class="nav-item"> <!-- LISTA USUARIOS -->
+                    <a class="nav-link active" aria-current="page" href="listaUsers.php">
+                        <i class="fa fa-user-plus" aria-hidden="true"></i>
+                        Usuarios
+                    </a>
+                </li>
+                <li class="nav-item"> <!-- LISTA PUBLICACIONES -->
+                    <a class="nav-link" aria-current="page" href="listaPublicaciones.php">
+                        <i class="fa-solid fa-file-pen"></i> 
+                        Publicaciones
+                    </a>
+                </li>
+                <li class="nav-item"> <!-- LISTA PANTALLAS -->
+                    <a class="nav-link" aria-current="page" href="listaPantallas.php">
+                        <i class="fa-solid fa-desktop"></i>
+                        Pantallas
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">  <!-- CERRAR SESIÓN -->
+                    <a class="nav-link active" aria-current="page" href="cerrar.php">
+                        <i class="fa fa-sign-in" aria-hidden="true"></i>
+                        Cerrar sesión
+                    </a>
+                </li>
+            </ul>
+            </div>
+        </div> 
+    </nav> 
+
+    
+
+
+
+    <section id="listas">
+
+
+
+
+
+        <h1>Estas en Usuarios</h1>
+
+    </section>
+
+
+    <footer class="fixed-bottom">
+        <img src="img/logo.webp" alt="logo" width="30" height="30" class="d-inline-block align-text-top">
+        <p>CPIFP Bajo Aragon</p>
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
